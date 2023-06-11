12fe9004e9d7552350f224a8e268d2a79393794b
James Lee <jamesrafe10@gmail.com>
2023-06-11 21:47:36 +0700

    Added flora-transfer model python file

---
+++ notebooks/flora-transfer.py
@@ -0,0 +1,138 @@
+# Import statements
+import os
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from keras import Model
+from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import img_to_array, load_img
+from keras.applications.inception_v3 import InceptionV3
+
+IMAGE_SIZE = 150
+
+def train_val_generators(TRAINING_DIR, VALIDATION_DIR):
+  # Train dataset will be augmented as follows
+  train_datagen = ImageDataGenerator(rescale=1./255,
+                                     rotation_range=40,
+                                     width_shift_range=0.2,
+                                     height_shift_range=0.2,
+                                     shear_range=0.2,
+                                     zoom_range=0.2,
+                                     horizontal_flip=True,
+                                     vertical_flip=True,
+                                    #  fill_mode='nearest'
+                                    )
+  # Initialize training generator
+  train_generator = train_datagen.flow_from_directory(directory=TRAINING_DIR,
+                                                      batch_size=32, 
+                                                      class_mode='categorical',
+                                                      target_size=(IMAGE_SIZE, IMAGE_SIZE))
+  
+  # Validation dataset will not be augmented 
+  validation_datagen = ImageDataGenerator(rescale=1./255)
+  # Initialize validation generator
+  validation_generator = validation_datagen.flow_from_directory(directory=VALIDATION_DIR,
+                                                                batch_size=32, 
+                                                                class_mode='categorical',
+                                                                target_size=(IMAGE_SIZE, IMAGE_SIZE))
+  return train_generator, validation_generator
+
+train_generator, validation_generator = train_val_generators('Fruits/train', 'Fruits/val')
+
+def create_pre_trained_model(local_weights_file):
+  pre_trained_model = InceptionV3(input_shape = (IMAGE_SIZE, IMAGE_SIZE, 3),
+                                  include_top = False, 
+                                  weights = None) 
+  pre_trained_model.load_weights(local_weights_file)
+
+  for layer in pre_trained_model.layers:
+    layer.trainable = False
+  
+  return pre_trained_model
+
+local_weights_file = 'inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5'
+pre_trained_model = create_pre_trained_model(local_weights_file)
+
+def output_of_last_layer(pre_trained_model):
+  last_desired_layer = pre_trained_model.get_layer('mixed7')
+  print('last layer output shape: ', last_desired_layer.output_shape)
+  last_output = last_desired_layer.output
+  print('last layer output: ', last_output)
+  return last_output
+
+last_output = output_of_last_layer(pre_trained_model)
+
+def create_final_model(pre_trained_model, last_output):
+  x = layers.Flatten()(last_output)
+
+  # x = layers.Dense(1024, activation='relu')(x)
+  # x = layers.Dropout(0.2)(x) 
+  x = layers.Dense(46, activation='softmax')(x)
+
+  # Create the complete model by using the Model class
+  model = Model(inputs=pre_trained_model.input, outputs=x)
+
+  # Compile the model
+  model.compile(optimizer = RMSprop(learning_rate=0.0001), 
+                loss = 'categorical_crossentropy',
+                metrics = ['accuracy'])
+  
+  return model
+
+model = create_final_model(pre_trained_model, last_output)
+# Inspect parameters
+total_params = model.count_params()
+num_trainable_params = sum([w.shape.num_elements() for w in model.trainable_weights])
+
+print(f"There are {total_params:,} total parameters in this model.")
+print(f"There are {num_trainable_params:,} trainable parameters in this model.")
+
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('accuracy')>0.99):
+      print("\nReached 99% accuracy so cancelling training!")
+      self.model.stop_training = True
+
+callbacks = myCallback()
+
+history = model.fit(train_generator,
+                    epochs=20,
+                    validation_data=validation_generator,
+                    callbacks=[callbacks])
+
+#-----------------------------------------------------------
+# Retrieve a list of list results on training and test data
+# sets for each training epoch
+#-----------------------------------------------------------
+acc      = history.history[     'accuracy' ]
+val_acc  = history.history[ 'val_accuracy' ]
+loss     = history.history[    'loss' ]
+val_loss = history.history['val_loss' ]
+
+print(acc)
+print(val_acc)
+print(loss)
+print(val_loss)
+
+epochs   = range(len(acc)) # Get number of epochs
+
+#------------------------------------------------
+# Plot training and validation accuracy per epoch
+#------------------------------------------------
+plt.plot  ( epochs,     acc, label='Training')
+plt.plot  ( epochs, val_acc, label='Validation')
+plt.title ('Training and validation accuracy')
+plt.legend()
+plt.figure()
+
+#------------------------------------------------
+# Plot training and validation loss per epoch
+#------------------------------------------------
+plt.plot  ( epochs,     loss, label='Training')
+plt.plot  ( epochs, val_loss, label='Validation')
+plt.legend()
+plt.title ('Training and validation loss')
+
+model.save("inception-model-fruitsflowersvege.h5")
